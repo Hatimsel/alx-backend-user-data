edf2bfc11338386181adc071758cb9c2ce6d06bd
Hatim Selmun <selmun13@gmail.com>
2024-05-30 11:39:06 +0100

    filtering values in incoming log records

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Logging in Python"""
+import logging
 import re
 from typing import List
 
@@ -13,3 +14,23 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = f"({'|'.join(fields)})=([^ {separator}]+)"
 
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records"""
+        org_msg = super().format(record)
+
+        return filter_datum(self.fields, self.REDACTION,
+                            org_msg, self.SEPARATOR)
