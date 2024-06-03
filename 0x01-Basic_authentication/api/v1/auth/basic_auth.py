d19f95863b54e4e175ee5e35f6167a28306000c6
Hatim Selmun <selmun13@gmail.com>
2024-06-03 16:46:41 +0100

    extracting credentials from the decoded header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -27,3 +27,15 @@ class BasicAuth(Auth):
             return decoded_auth.decode('utf-8')
         except (TypeError, ValueError):
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """Extract user credentials"""
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        email = decoded_base64_authorization_header.split(':')[0]
+        password = decoded_base64_authorization_header.split(':')[1]
+
+        return (email, password)
