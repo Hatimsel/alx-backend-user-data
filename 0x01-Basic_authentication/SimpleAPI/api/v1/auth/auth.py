4c45e24ee32ec120b6f98a2377b4680e035c3acc
Hatim Selmun <selmun13@gmail.com>
2024-06-03 12:29:42 +0100

    require auth implementation

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -6,9 +6,13 @@ from typing import List, TypeVar
 
 class Auth:
     """Class Auth"""
-    def require_auth(self, path: str, excluded_paths:List[str]) -> bool:
-        """Checks is a path requires authentication"""
-        return False
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication"""
+        if path is None or excluded_paths is None:
+            return True
+        elif path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Adds the Authorization header to the request"""
