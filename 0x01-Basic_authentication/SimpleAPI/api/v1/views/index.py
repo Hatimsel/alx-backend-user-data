37b8b0a2e0c4b1cdb0e39ce8f449f073fe2c9725
Hatim Selmun <selmun13@gmail.com>
2024-06-03 12:01:05 +0100

    unauthorized error handler

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'],
+                 strict_slashes=False)
+def raise_unauthorized():
+    """Raise unauthorized error"""
+    abort(401)
