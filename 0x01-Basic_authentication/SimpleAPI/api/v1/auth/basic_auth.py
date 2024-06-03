a2304fedbc9660a65070fda9c508f671c4a6b2c4
Hatim Selmun <selmun13@gmail.com>
2024-06-03 16:32:22 +0100

    decoding the auth header

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """BasicAuth class"""
 from .auth import Auth
-#from auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Basic Auth class"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
         """Extract the Authorization header"""
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None or type(authorization_header)\
+                is not str:
             return None
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """Decode base64 authorization header"""
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_auth = base64.b64decode(base64_authorization_header.
+                                            encode('utf-8'))
+            return decoded_auth.decode('utf-8')
+        except (TypeError, ValueError):
+            return None
