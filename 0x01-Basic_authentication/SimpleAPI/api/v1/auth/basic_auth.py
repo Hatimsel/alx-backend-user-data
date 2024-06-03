90d902a1de926adf5e87fef286bded463f7d24be
Hatim Selmun <selmun13@gmail.com>
2024-06-03 15:59:09 +0100

    extracting the base64 auth string

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -6,4 +6,10 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """Basic Auth class"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extract the Authorization header"""
+        if authorization_header is None or type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]
