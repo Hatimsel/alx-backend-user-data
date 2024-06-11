9681814aaa5c2dc28df9e39bf8f0e14ee57bda1f
Hatim Selmun <selmun13@gmail.com>
2024-06-11 19:11:39 +0100

    add a user to the database

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """User class"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
+from uuid import uuid4
 
 Base = declarative_base()
 
@@ -10,9 +10,25 @@ Base = declarative_base()
 class User(Base):
     """Class User Mapped to table users"""
     __tablename__ = 'users'
+    # ids = 0
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    # @property
+    # def incr_ids(self):
+    #     """Increments ids"""
+    #     self.ids += 1
+
+    # def __init__(self, email: str, hashed_password: str,
+    #              session_id: str=None, reset_token: str =None):
+    #     """Instantiate an new instnace of the class"""
+    #     self.id = self.ids + 1
+    #     self.incr_ids
+    #     self.email = email
+    #     self.hashed_password = hashed_password
+    #     self.session_id = session_id
+    #     self.reset_token = reset_token
